a49528af1044733107717a76ab4a493378cd21ee
deviato <deviato@gmail.com>
2024-05-25 17:07:44 +0200

    Droid PPPwn 1.0

--- app/src/main/java/it/deviato/droidpppwn/MainActivity.java
+++ app/src/main/java/it/deviato/droidpppwn/MainActivity.java
@@ -2,6 +2,7 @@ package it.deviato.droidpppwn;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -15,6 +16,7 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -24,9 +26,11 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout main;
     private TextView txtOut;
     private Spinner spnFW;
     private Process p;
+    private AsyncExec ae;
     protected SharedPreferences settings;
     protected SharedPreferences.Editor editset;
     protected int selFw;
@@ -45,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         settings=this.getPreferences(Context.MODE_PRIVATE);
         editset=settings.edit();
         selFw=settings.getInt("FW",0);
+        main=findViewById(R.id.main);
         txtOut=findViewById(R.id.txtOutput);
         txtOut.setMovementMethod(new ScrollingMovementMethod());
         spnFW=findViewById(R.id.spnFirmware);
@@ -64,20 +69,20 @@ public class MainActivity extends AppCompatActivity {
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(btnStart.isChecked()) startExploit();
+                if(btnStart.isChecked()) {
+                    txtOut.setText("----------[Exploit-Started]----------"+EOL);
+                    ae=new AsyncExec();
+                    ae.execute();               }
                 else {
+                    ae.cancel(true);
                     p.destroy();
                     txtOut.setText("");
+                    main.setBackgroundColor(Color.WHITE);
                 }
             }
         });
     }
 
-    protected void startExploit() {
-        txtOut.setText("----------[Exploit-Started]----------"+EOL);
-        AsyncExec ae=new AsyncExec();
-        ae.execute();
-    }
     private class AsyncExec extends AsyncTask<String,Void,String> {
         @Override
         protected String doInBackground(String... strings) {
@@ -86,18 +91,18 @@ public class MainActivity extends AppCompatActivity {
             String fw=fws[spnFW.getSelectedItemPosition()];
             try {
                 String path=getApplicationInfo().nativeLibraryDir+"/";
-                Log.d("Droid","Attempting to run: "+path+"pppwnarm");
+                //Log.d("Droid","Attempting to run: "+path+"pppwn");
                 p=Runtime.getRuntime().exec("su");
                 //p=Runtime.getRuntime().exec("su -c ping www.google.it");
                 DataOutputStream os=new DataOutputStream(p.getOutputStream());
                 os.writeBytes("/system/bin/ifconfig eth0 10.0.0.1 up 2>&1\n");
                 os.flush();
-                os.writeBytes("export PATH="+path+":$PATH\nLD_LIBRARY_PATH="+path+" "); //LD_PRELOAD="+path+"libpcap.so.1 ");
-                os.writeBytes("pppwnarm -i eth0 --fw "+fw+" --stage1 stage1."+fw+" --stage2 stage2."+fw+" -a 2>&1\nexit\n");
+                os.writeBytes("export PATH=$PATH:"+path+"\nLD_LIBRARY_PATH=$LD_LIBRARY_PATH:"+path+" "); //LD_PRELOAD="+path+"libpcap.so.1 ");
+                os.writeBytes("pppwn -i eth0 --fw "+fw+" --stage1 "+path+"stage1."+fw+" --stage2 "+path+"stage2."+fw+" -a 2>&1\nexit\n");
                 os.flush();
                 BufferedReader in=new BufferedReader(new InputStreamReader(p.getInputStream()));
                 while((line=in.readLine())!=null) {
-                    publishProgress(line+EOL);
+                    publishProgress(line);
                 }
                 in.close();
                 p.waitFor();
@@ -108,12 +113,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         private void publishProgress(String str) {
-            txtOut.append(str);
-            try {
-                Thread.sleep(10);  //Fix java.lang.IndexOutOfBoundsException: offset(x) should be less than line limit(0)
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    txtOut.append(str+EOL);
+                    Log.d("Droid",str);
+                    if(str.equals("[+] Done!")) main.setBackgroundColor(Color.parseColor("#55ff55"));
+                }
+            });
         }
     }
 }
